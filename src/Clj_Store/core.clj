3cc632e51690bc5cc5615e32548654dc3ac2c562
MayDaniel <yamnad1@gmail.com>
2010-05-26 17:49:49 +0100

    Redesigned the way in which Clj-Store is used.

--- src/Clj_Store/core.clj
+++ src/Clj_Store/core.clj
@@ -5,74 +5,82 @@
 ;; "s-" is used a prefix to avoid definition conflicts.
 ;; The paramater "file" may be absolute or relative.
 
+(declare *store*)
+
 (defn s-new
   "Creates a new file with the specified name."
   [file]
-  (.createNewFile (File. file)))
+  (if (false? (.exists (File. file)))
+    (-> file File. .createNewFile)))
+
+(defmacro with-store
+  "Creates - if one does not already exist - a new file, and evaluates
+  the body."
+  [file & body]
+  (s-new conf)
+  `(binding [*store* ~file]
+     ~@body))
 
 (defn s-read
   "Reads the map inside the file."
   [file]
-  (-> file slurp* read-string))
+  (let [f (-> file slurp*)]
+    (if (empty? f) {} (read-string f))))
 
 (defn s-keys
   "Returns a sequence of the keys in the file."
-  [file]
-  (-> file s-read keys))
+  []
+  (-> *store* s-read keys))
 
 (defn s-vals
   "Returns a sequence of the values in the file."
-  [file]
-  (-> file s-read vals))
+  []
+  (-> *store* s-read vals))
 
 (defn s-get
   "Returns the value of a single key."
-  [file key]
-  (-> file s-read key))
+  [key]
+  (-> *store* s-read key))
 
 (defn s-get-in
   "See get-in."
-  [file ks]
-  (reduce get (s-read file) ks))
+  [ks]
+  (reduce get (s-read *store*) ks))
 
 (defn s-assoc
   "Adds a single key to a file."
-  [file key val]
-  (let [f (ref (s-read file))]
+  [key val]
+  (let [f (ref (s-read *store*))]
     (dosync (alter f assoc key val)
-	    (spit file @f))))
+	    (spit *store* @f))))
 
 (defn s-assoc-in
   "See assoc-in."
-  [file [k & ks] v]
+  [[k & ks] v]
   (let [key-args (into [k] ks)
-	contents (ref (s-read file))]
+	contents (ref (s-read *store*))]
     (dosync (alter contents merge (assoc-in @contents key-args v)))
-    (spit file @contents)))
+    (spit *store* @contents)))
 
 (defn s-dissoc
   "Removes a single key from a file."
-  [file key]
-  (let [f (ref (s-read file))]
+  [key]
+  (let [f (ref (s-read *store*))]
     (dosync (alter f dissoc key)
-	    (spit file @f))))
+	    (spit *store* @f))))
 
 (defn s-add
   "Inserts new key/vals or updates the value of already existing ones
   an arbitrary number of key/vals. If :create? is true, and a file 
   with the name does not already exist in the directory then a new 
   file will be created."
-  [file hmap & [{:keys [create?] :or {create? false}}]]
-  (if (and (true? create?) (false? (.exists (File. file)))) (spit (File. file) hmap)
-      (spit file 
-	    (merge 
-	     (s-read file)
-	     hmap))))
+  [hmap]  
+  (spit *store* (merge (s-read *store*) hmap)))
 
 (defn s-update
   "Updates the value of already existing keys."
-  [file hmap]
-  (let [contents (ref (s-read file))
+  [hmap]
+  (let [contents (ref (s-read *store*))
 	common-keys (ref (filter #(contains? @contents (key %)) hmap))]
     (dosync (alter contents merge @common-keys))
-    (spit file @contents)))
+    (spit *store* @contents)))
