ddf8232aac8c4b8f76d4e52a4b0ef3797d915f9d
MayDaniel <yamnad1@gmail.com>
2010-06-05 00:08:59 +0100

    Fixed a few bugs cononcerning the macro.

--- src/Clj_Store/core.clj
+++ src/Clj_Store/core.clj
@@ -9,25 +9,25 @@
 
 (defn s-new
   "Creates a new store or file."
-  ([] (s-new *store*))
   ([file]
-  (if (false? (.exists (File. file)))
-    (-> file File. .createNewFile))))
+  (when (false? (.exists (File. file)))
+    (-> file File. .createNewFile)
+    (spit file "")) file))
+
+(defn s-read
+  "Reads the map inside the store or a file."
+  ([] (s-read @*store*))
+  ([file]
+  (let [f (-> file slurp*)]
+    (if (empty? f) {} (read-string f)))))
 
 (defmacro with-store
   "Creates - if one does not already exist - a new file, and evaluates
   the body."
   [file & body]
-  `(binding [*store* ~file]
-  (s-new)
-     ~@body))
-
-(defn s-read
-  "Reads the map inside the store or a file."
-  ([] (s-read *store*))
-  ([file]
-  (let [f (-> file slurp*)]
-    (if (empty? f) {} (read-string f)))))
+  `(binding [*store* (ref (s-read (s-new ~file)))]
+  ~@body
+  (spit ~file @*store*)))
 
 (defn s-clean
   "Removes the contents of a store or a file."
@@ -38,55 +38,47 @@
 (defn s-keys
   "Returns a sequence of the keys in the store."
   []	
-  (-> *store* s-read keys))
+  (-> @*store* s-read keys))
 
 (defn s-vals
   "Returns a sequence of the values in the store."
   []
-  (-> *store* s-read vals))
+  (-> @*store* s-read vals))
 
 (defn s-get
   "Returns the value of a single key."
   [key]
-  (-> *store* s-read key))
+  (-> @*store* s-read key))
 
 (defn s-get-in
   "See get-in."
   [ks]
-  (reduce get (s-read *store*) ks))
+  (reduce get (s-read @*store*) ks))
 
 (defn s-assoc
   "Adds a single key to the store."
   [key val]
-  (let [f (ref (s-read *store*))]
-    (dosync (alter f assoc key val)
-	    (spit *store* @f))))
+  (dosync (alter *store* assoc key val)))
 
 (defn s-assoc-in
   "See assoc-in."
   [[k & ks] v]
-  (let [key-args (into [k] ks)
-	contents (ref (s-read *store*))]
-    (dosync (alter contents merge (assoc-in @contents key-args v)))
-    (spit *store* @contents)))
+  (let [key-args (into [k] ks)]
+    (dosync (alter *store* merge (assoc-in @*store* key-args v)))))
 
 (defn s-dissoc
   "Removes a single key from the store."
   [key]
-  (let [f (ref (s-read *store*))]
-    (dosync (alter f dissoc key)
-	    (spit *store* @f))))
+  (dosync (alter *store* dissoc key)))
 
 (defn s-add
   "Inserts new key/vals or updates the value of already existing ones
   an arbitrary number of key/vals."
   [hmap]  
-  (spit *store* (merge (s-read *store*) hmap)))
+  (dosync (alter *store* merge hmap)))
 
 (defn s-update
   "Updates the value of keys already existing within the store."
   [hmap]
-  (let [contents (ref (s-read *store*))
-	common-keys (ref (filter #(contains? @contents (key %)) hmap))]
-    (dosync (alter contents merge @common-keys))
-    (spit *store* @contents)))
+  (let [common-keys (ref (filter #(contains? @*store* (key %)) hmap))]
+    (dosync (alter *store* merge @common-keys))))
