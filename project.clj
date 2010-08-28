79242317263f8d7904bae06bee30e3ee512f7049
MayDaniel <yamnad1@gmail.com>
2010-08-28 19:21:23 +0100

    Foobarbaz.

--- project.clj
+++ project.clj
@@ -3,5 +3,4 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]]
-  :namespaces [clj-store.core])
+                     [lein-clojars "0.5.0"]])
